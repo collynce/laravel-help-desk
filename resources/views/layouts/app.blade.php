795ae994585d4fa5c719ae38f43045949162cc1d
Collins Kiprotich <collinsrono02@gmail.com>
2020-01-30 00:33:30 +0300

    Updated Ticket Generation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
             </svg>
             <span class="font-semibold text-xl tracking-tight"><a href="{{ url('/') }}">CyHelp</a></span>
         </div>
-        <div class="block lg:hidden">
+        <div class="block lg:hidden ">
             <button
                 class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
                 <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>
